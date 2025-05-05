bbda7e5ace33b845636d51c43533a1a5c70b9fe6
Ivan-PIA <wind.vanya@gmail.com>
2025-05-05 20:20:32 +0700

    update main: add size, full enter

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@
 int main() {
     using Complex = std::complex<double>;
 
-    const size_t N = 128;  // длина, кратная 2, 3, 5
+    int N = 21;  // длина, кратная 2 3 5
     std::vector<Complex> data(N);
 
-    std::mt19937 rng(123);  // фиксируем seed
+    std::mt19937 rng(1);  // фиксируем сид
     std::uniform_int_distribution<int> bit_dist(0, 1);
 
-    // QPSK mapping: 2 бита -> 1 комплексная точка
+    // QPSK 
     auto bits_to_qpsk = [](int b1, int b0) -> Complex {
         double norm = std::sqrt(0.5);
         double real = (b1 ? -1 : 1);
@@ -24,39 +24,37 @@ int main() {
         return Complex(real, imag) * norm;
     };
 
-    // Генерация N QPSK символов
+    
     for (size_t i = 0; i < N; ++i) {
         int b1 = bit_dist(rng);
         int b0 = bit_dist(rng);
         data[i] = bits_to_qpsk(b1, b0);
     }
 
-    // Печать QPSK сигнала
-    std::cout << "QPSK сигнал:\n";
+    std::cout << "\nSize = " << N << "\n";
+
+    std::cout << "\nQPSK сигнал:\n";
     for (const auto& x : data) {
-        std::cout << std::fixed << std::setprecision(4)
-                  << "(" << x.real() << ", " << x.imag() << "i)\n";
+        std::cout << "(" << x.real() << ", " << x.imag() << "i)\n";
     }
 
     // FFT
     
-    std::vector<Complex> data_fft = FFT::compute(data, false);
+    std::vector<Complex> data_ifft = FFT::compute(data, false);
 
     // Печать спектра (амплитуда)
-    std::cout << "\nСпектр (амплитуды):\n";
-    for (const auto& x : data_fft) {
-        std::cout << std::fixed << std::setprecision(4)
-                  << "(" << x.real() << ", " << x.imag() << "i)\n";
+    std::cout << "\nFFT:\n\n";
+    for (const auto& x : data_ifft) {
+        std::cout <<  "(" << x.real() << ", " << x.imag() << "i)\n";
     }
 
-    
-    std::vector<Complex> data_ifft = FFT::compute(data_fft, true);
+    // IFFT
+    std::vector<Complex> data_fft = FFT::compute(data_ifft, true);
 
-    std::cout << "demod QPSK сигнал:\n";
+    std::cout << "\nIFFT:\n\n";
     
-    for (const auto& x : data_ifft) {
-        std::cout << std::fixed << std::setprecision(4)
-                  << "(" << x.real() << ", " << x.imag() << "i)\n";
+    for (const auto& x : data_fft) {
+        std::cout << "(" << x.real() << ", " << x.imag() << "i)\n";
     }
 
     return 0;
