7805bf0684f18f9e004ae94ec51dce2eb5a785a2
Ivan-PIA <wind.vanya@gmail.com>
2025-05-07 03:19:00 +0700

    add choise Google test/not test

--- build_run.sh
+++ build_run.sh
@@ -1,6 +1,19 @@
-mkdir build 
-cd build
-cmake ..
-make
+#!/bin/bash
 
-./fft
+set -e 
+
+MODE=${1:-run} 
+
+BUILD_DIR=build
+
+if [ "$MODE" = "test" ]; then
+    echo "➡️  Building with tests enabled..."
+    cmake -B $BUILD_DIR -DBUILD_TESTS=ON
+    cmake --build $BUILD_DIR
+    $BUILD_DIR/test_fft
+else
+    echo "➡️  Building main program..."
+    cmake -B $BUILD_DIR
+    cmake --build $BUILD_DIR
+    $BUILD_DIR/fft_main
+fi
