f4db9d044c77e4b1ad3a2d07c5b069b2e8829b52
Ivan-PIA <wind.vanya@gmail.com>
2025-05-07 03:34:28 +0700

    debag choise test/not test

--- build_run.sh
+++ build_run.sh
@@ -7,12 +7,12 @@ MODE=${1:-run}
 BUILD_DIR=build
 
 if [ "$MODE" = "test" ]; then
-    echo "➡️  Building with tests enabled..."
+    echo "--- Building with tests enabled..."
     cmake -B $BUILD_DIR -DBUILD_TESTS=ON
     cmake --build $BUILD_DIR
     $BUILD_DIR/test_fft
 else
-    echo "➡️  Building main program..."
+    echo "--- Building main program..."
     cmake -B $BUILD_DIR
     cmake --build $BUILD_DIR
     $BUILD_DIR/fft_main
